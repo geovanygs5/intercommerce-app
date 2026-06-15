6a99679010a0a283ffda466d8748d8e6daa464db
Geovany <dgeo2gs@gmail.com>
2026-06-14 22:04:54 -0500

    renombrando componentes 2

--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -1,7 +1,7 @@
 import { useState, useCallback } from 'react';
 import { useProducts, useFilters } from '../hooks/useProducts';
 import { useCartStore } from '../store/cartStore';
-import { ProductGrid } from '../components/product/productgrid';
+import { Productgrid } from '../components/product/productgrid';
 import { FilterBar } from '../components/product/FilterBar';
 import { Toast, useToast } from '../components/ui/Toast';
 import { ErrorBoundary } from '../components/ui/ErrorBoundary';
@@ -43,7 +43,7 @@ export const ProductsPage = () => {
 
         <FilterBar />
 
-        <ProductGrid
+        <Productgrid
           products={data?.products ?? []}
           isLoading={isLoading}
           onAddToCart={handleAddToCart}
